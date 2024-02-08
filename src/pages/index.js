ada28384b41a7343f917841f2bc33e03faefafd5
jmoralesDev <jm.pantunfla12@gmail.com>
2024-02-08 08:41:12 -0600

    fixed video size

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,47 +1,30 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-import { Box } from "@mui/material";
-
-const inter = Inter({ subsets: ["latin"] });
+import React from "react";
+import YouTube from "react-youtube";
 
 export default function Home() {
+  const opts = {
+    height: "100%",
+    width: "1200px",
+    playerVars: {
+      autoplay: 1,
+      controls: 1,
+      loop: 1,
+      showinfo: 0,
+      playlist: "gN5hj3vXMX8",
+    },
+  };
+
   return (
-    <Box
-      width="100vw"
-      height="100vh"
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        overflow: "hidden",
+        width: "100vw",
+        height: "100vh",
+      }}
     >
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          width: "300px",
-          height: "533px",
-          // position: "relative",
-          overflow: "hidden",
-          border: 0,
-        }}
-      >
-        <iframe
-          sx={{
-            width: "100%",
-            height: "100%",
-            position: "absolute",
-            top: 0,
-            left: 0,
-            border: 0,
-          }}
-          src="https://www.youtube.com/embed/gN5hj3vXMX8?autoplay=1&loop=1&controls=0&showinfo=0&playlist=gN5hj3vXMX8"
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </Box>
-    </Box>
+      <YouTube videoId="gN5hj3vXMX8" opts={opts} />
+    </div>
   );
 }
